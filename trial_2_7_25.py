4a451d174cc1d98e027b5e86a22b385cf2fcca20
leoflur <156014211+leoflur@users.noreply.github.com>
2025-02-15 16:01:09 -0800

    i dont even know

--- trial_2_7_25.py
+++ trial_2_7_25.py
@@ -58,7 +58,7 @@ def process_eeg_files(folder_path):
                 raw_eeg, events=events, event_id=event_id,
                 tmin=-0.6, tmax=0.6, 
                 picks = "eeg",
-                baseline=None, preload=True
+                baseline= (-0.6,0), preload=True
             )
             
             conditions = ['frequent_rare', 'rare_frequent', 'frequent_frequent']
@@ -70,38 +70,61 @@ def process_eeg_files(folder_path):
             
             for electrode in electrodes:  
                 for condition in conditions:
-                    # 5. Separate Epochs into Pre/Post (inlined from separate_epochs)
-                    epochs_cond = epochs[condition].copy().pick_channels([electrode])
-                    epochs_pre = epochs_cond.copy().crop(tmin=-0.6, tmax=0)
-                    epochs_post = epochs_cond.copy().crop(tmin=0, tmax=0.6)
+                    # # 5. Separate Epochs into Pre/Post (inlined from separate_epochs)
+                    # epochs_cond = epochs[condition].copy().pick_channels([electrode])
+                    # epochs_pre = epochs_cond.copy().crop(tmin=-0.6, tmax=0)
+                    # epochs_post = epochs_cond.copy().crop(tmin=0, tmax=0.6)
                     
-                    # Debug: Check epoch counts
-                    print(f"Condition {condition}: {len(epochs_pre)} pre and {len(epochs_post)} post epochs")
-                    assert len(epochs_pre) == len(epochs_post)
+                    # # Debug: Check epoch counts
+                    # print(f"Condition {condition}: {len(epochs_pre)} pre and {len(epochs_post)} post epochs")
+                    # assert len(epochs_pre) == len(epochs_post)
                     
-                    # 6. Perform FFT on Pre/Post (inlined from perform_fft_on_epochs)
-                    pre_data = epochs_pre.get_data()[:, 0, :]  # Shape: (n_epochs, n_samples)
-                    post_data = epochs_post.get_data()[:, 0, :]
+                    # # 6. Perform FFT on Pre/Post (inlined from perform_fft_on_epochs)
+                    # pre_data = epochs_pre.get_data()[:, 0, :]  # Shape: (n_epochs, n_samples)
+                    # post_data = epochs_post.get_data()[:, 0, :]
+
+                    # Below is a an improved approach where we use boolean masking instead of having to create multiple copeis all the time
+                    # Get electrodes for the condition
+                    epochs_data = epochs[condition].get_data() # Shape : ( n_epocs, n_channels, n_times)
+                    times = epochs.times # Time vector from original epochs
+
+                    # Find electrode index
+                    electrode_idx = epochs.ch_names.index(electrode)
+
+                    # Extract data for this electrode
+                    electrode_data = epochs_data[:,electrode_idx, : ]
                     
-                    pre_spectra = np.abs(fft(pre_data, axis=1))
-                    post_spectra = np.abs(fft(post_data, axis=1))
+                    # Create time masks
+                    pre_mask = (times >= -0.6) & (times <= 0)
+                    post_mask = (times >= 0) & (times <= 0.6)
+
+                    # Split into pre and post
+                    pre_data = electrode_data[:, pre_mask]  # Shape: (n_epochs, n_pre_samples)
+                    post_data = electrode_data[:, post_mask]
+
+                    print(f"Condition {condition}: {pre_data.shape[0]} pre and {post_data.shape[0]} post epochs")
+                    assert pre_data.shape[0] == post_data.shape[0]
                     
+                    # Compute FFT on pre/post data
+                    pre_spectra = np.abs(np.fft.fft(pre_data, axis=1))  # FFT along time axis
+                    post_spectra = np.abs(np.fft.fft(post_data, axis=1))
+                    
+                    # Average spectra across epochs
                     yf_avg_pre = pre_spectra.mean(axis=0)
                     yf_avg_post = post_spectra.mean(axis=0)
                     
-                    # 7. Compute ERP (inlined from compute_erp)
-                    erp = epochs_post.average()  # Fix: Use epochs_post directly
+                    # Compute ERP (average across epochs)
+                    erp = post_data.mean(axis = 0 ) # Shape: (n_post_samples,)
                     
-                    # 8. FFT on ERP (inlined from perform_fft_on_erp)
-                    erp_data = erp.pick([electrode]).get_data()[0, :]  # Shape: (n_samples,)
-                    fft_erp = np.abs(fft(erp_data))
+                    # FFT on ERP (inlined from perform_fft_on_erp)
+                    fft_erp = np.abs(np.fft.fft(erp))
+
+                    # Get frequency axis (using origina sampling rate)
+                    sample_rate = epochs.info['sfreq'] # Get SF from original epochs
+                    N_erp = pre_data.shape[1] # Number of time samples in pre
+                    xf = np.fft.fftfreq(N_erp, 1 / sample_rate)
                     
-                    # 9. Get Frequency Axes (inlined from get_xf)
-                    sample_rate = epochs_pre.info['sfreq']
-                    N_pre = epochs_pre.get_data().shape[-1]
-                    xf = fftfreq(N_pre, 1/sample_rate)
-                    
-                    N_erp = len(erp.times)
+                    N_erp = post_data.shape[1] # Number of time sampels in post-data
                     xf_erp = fftfreq(N_erp, 1/sample_rate)
                     
                     # Debug: Check frequency axis shapes
@@ -110,6 +133,9 @@ def process_eeg_files(folder_path):
                     
                     # 10. Post-minus-ERP Calculation
                     post_minus_erp = yf_avg_post - fft_erp
+
+                    #need to add in a np.save here for all the power spectral data 
+                    # compile them into 
                     
                     # 11. FOOOF Analysis
                     power_spec_windows = [
@@ -131,7 +157,21 @@ def process_eeg_files(folder_path):
                             peak_threshold=2.0,
                             aperiodic_mode='fixed'
                         )
+                        
                         fg.fit(xf, power_spec, freq_range=(2, 25))
+
+                        try:
+                            fg.fit(xf, power_spec, freq_range=(2, 25))
+                            if fg.has_model:
+                                exp = fg.get_params('aperiodic_params')[1]
+                                offset = fg.get_params('aperiodic_params')[0]
+                                rsq = fg.get_params('r_squared')
+                            else:
+                                exp, offset, rsq = np.nan, np.nan, np.nan
+                        except:
+                            exp, offset, rsq = np.nan, np.nan, np.nan
+
+                        #if fg.has_model is False give NaNs values otherwise keep script the same
 
                         new_row['Time Window'] = name
                         new_row['Exp'] =  fg.get_params('aperiodic_params')[1]  # Index 1 corresponds to the exponent
@@ -157,6 +197,8 @@ def process_eeg_files(folder_path):
 
 if __name__ == '__main__':
     folder_path = "C:/Users/leofl/OneDrive/Pictures/Documents/GitHub/Project-Cygnus/files"
-    y_frequencies_dict = process_eeg_files(folder_path)
-
-print(y_frequencies_dict)
+    y_frequencies_df = process_eeg_files(folder_path)
+    y_frequencies_df.to_csv("Z:/LeoF/Cygnus/y_frequencies_df.csv")
+    # this is where np.save will go 
+    
+print(y_frequencies_df)
