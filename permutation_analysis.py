90d55c21b8c94d98fb893d95cad31d8cce7fec32
leoflur <156014211+leoflur@users.noreply.github.com>
2025-03-09 14:02:07 -0700

    renamed trial to df creation and added comments to permutation analysis

--- permutation_analysis.py
+++ permutation_analysis.py
@@ -5,27 +5,27 @@ import pandas as pd
 import scipy.stats
 import os
 
-n_permutations = 10000
-n_swap = 0.5
+n_permutations = 10000 # Number of permutations for statistical testing
+n_swap = 0.5 # Fraction of data to shuffle 
 
-df_dir = "Y:\\LeoF\\Cygnus\\DataFrame\\"
+df_dir = "Z:/LeoF/Cygnus/DataFrame/"
 
-exps = pd.read_csv(df_dir+"data_frame_example.csv")
+exps = pd.read_csv(df_dir+"data_frame.csv")
 
-exps_pre = exps[exps['Time Window'] == 'yf_avg_pre']
-exps_post = exps[exps['Time Window'] == 'yf_avg_post']
+exps_pre = exps[exps['Time Window'] == 'yf_avg_pre'] # Pre exps
+exps_post = exps[exps['Time Window'] == 'yf_avg_post'] # Post exps
 
 exps_pre = exps_pre.sort_values(by=['Participant', 'Electrode', 'Condition']).reset_index()
 exps_post = exps_post.sort_values(by=['Participant', 'Electrode', 'Condition']).reset_index()
 
 compared = exps_pre[['Participant', 'Electrode', 'Condition']].compare(other = exps_post[['Participant', 'Electrode', 'Condition']], keep_equal=False) 
-print(compared)
-assert compared.empty == True
+print(compared) # Ensuring no mismatches
+assert compared.empty == True # Should be empty if no mismatches
 
-exps_post['post_minus_pre_Exponent'] = (exps_post['Exponent'] - exps_pre['Exponent'])
+exps_post['post_minus_pre_Exponent'] = (exps_post['Exponent'] - exps_pre['Exponent']) # Compute post-pre differences
 exps_post['post_minus_pre_Offset'] = (exps_post['Offset'] - exps_pre['Offset'])
 
-grpby = exps_post.groupby(['Participant','Condition']).mean(['post_minus_pre_Exponent','post_minus_pre_Offset']).reset_index()
+grpby = exps_post.groupby(['Participant','Condition']).mean(['post_minus_pre_Exponent','post_minus_pre_Offset']).reset_index() # Aggregate data
 
 ### On each permutation, and within each condition
 ### 1) get the mean diff for each params, avg across electrodes, one point each subject
@@ -36,28 +36,28 @@ grpby = exps_post.groupby(['Participant','Condition']).mean(['post_minus_pre_Exp
 permuted_df = pd.DataFrame()
 for condition in ['frequent_rare', 'frequent_frequent', 'rare_frequent']:
 
-    temp_dict = {'condition' : ([condition]*n_permutations)}
+    temp_dict = {'condition' : ([condition]*n_permutations)} # E.g. condition = 'frequent_rare', n_permutations
 
-    grpby_this_cond = grpby[grpby['Condition'] == condition]
-    n_rows = len(grpby_this_cond)
-    n_rows_to_shuffle = int( n_rows*n_swap) # shuffle half of subjects
+    grpby_this_cond = grpby[grpby['Condition'] == condition] # Filter for rows of current conditon 
+    n_rows = len(grpby_this_cond) # 20 rows
+    n_rows_to_shuffle = int( n_rows*n_swap) # shuffle half of subjects (20* 0.5 = 10)
 
-    exps_intact = grpby_this_cond['post_minus_pre_Exponent'].values
+    exps_intact = grpby_this_cond['post_minus_pre_Exponent'].values # Return np arrays
     offs_intact = grpby_this_cond['post_minus_pre_Offset'].values
 
     perms_exps = []
     perms_offs = []
-    for p in range(n_permutations): 
+    for p in range(n_permutations): # 1000 iterations
 
-        perms_idx = np.random.permutation(n_rows)
-        perms_idx_to_flip = perms_idx[:n_rows_to_shuffle]
-        perms_idx_to_leave = perms_idx[n_rows_to_shuffle: ]
+        perms_idx = np.random.permutation(n_rows) # Permute len index of contiion
+        perms_idx_to_flip = perms_idx[:n_rows_to_shuffle]# Get first half
+        perms_idx_to_leave = perms_idx[n_rows_to_shuffle: ] # Get second half
 
         exps_sign_flip = (exps_intact[perms_idx_to_flip]*-1)
         exps_no_sign_flip = exps_intact[perms_idx_to_leave]
         permuted_exp = np.mean(np.hstack([exps_sign_flip, exps_no_sign_flip]))
 
-        offs_sign_flip = (offs_intact[perms_idx_to_flip]*-1)
+        offs_sign_flip = (offs_intact[perms_idx_to_flip]*-1) # Filter by array of indices (this is legal apparently)
         offs_no_sign_flip = offs_intact[perms_idx_to_leave]
         permuted_off = np.mean(np.hstack([offs_sign_flip, offs_no_sign_flip]))
 
